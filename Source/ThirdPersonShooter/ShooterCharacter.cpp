252c9a4c0eb399872d58d6d65aec0daeccffb129
AlvaroBishop <bishopgamma@gmail.com>
2023-01-13 00:45:16 -0600

    add input binding

--- Source/ThirdPersonShooter/ShooterCharacter.cpp
+++ Source/ThirdPersonShooter/ShooterCharacter.cpp
@@ -29,6 +29,18 @@ void AShooterCharacter::Tick(float DeltaTime)
 void AShooterCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
+	PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &AShooterCharacter::MoveForward);
+	PlayerInputComponent->BindAxis(TEXT("LookUp"), this, &APawn::AddControllerPitchInput);
 }
+
+void AShooterCharacter::MoveForward(float AxisValue)
+{
+	AddMovementInput(GetActorForwardVector() * AxisValue);
+}
+
+// void AShooterCharacter::LookUp(float AxisValue)
+// {
+// 	AddControllerPitchInput(AxisValue);
+// }
+
 
